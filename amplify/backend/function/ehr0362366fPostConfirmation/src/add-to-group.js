5d18fca6be96f8faf8d94c5292ec64c251fd65e6
Vinay KP <vinaykp@vinays-mbp.attlocal.net>
2019-07-28 12:29:35 -0500

    Update custom attribute with IAM policy changes

--- amplify/backend/function/ehr0362366fPostConfirmation/src/add-to-group.js
+++ amplify/backend/function/ehr0362366fPostConfirmation/src/add-to-group.js
@@ -1,7 +1,8 @@
-/* eslint-disable-line */ const aws = require('aws-sdk');
+/* eslint-disable-line */ 
+const aws = require('aws-sdk');
 
 exports.handler = async (event, context, callback) => {
-  const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' });
+  const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({ region: 'us-east-1',apiVersion: '2016-04-18' });
   const groupParams = {
     GroupName: process.env.GROUP,
     UserPoolId: event.userPoolId,
@@ -12,18 +13,36 @@ exports.handler = async (event, context, callback) => {
     UserPoolId: event.userPoolId,
     Username: event.userName,
   };
-
+  const addTenantDefault = {
+    UserAttributes: [
+      {
+        Name: 'custom:tier',
+        Value: 'Free'
+      },
+      {
+        Name: 'custom:tenantId',
+        Value: Math.floor(100000 + Math.random() * 900000)
+      }
+    ],
+    UserPoolId: event.userPoolId,
+    Username: event.userName
+  };
+  
   await cognitoidentityserviceprovider.getGroup(groupParams, async (err) => {
     if (err) {
       await cognitoidentityserviceprovider.createGroup(groupParams).promise();
     }
+    await cognitoidentityserviceprovider.adminUpdateUserAttributes(addTenantDefault).promise();
   }).promise();
 
 
   cognitoidentityserviceprovider.adminAddUserToGroup(addUserParams, (err) => {
     if (err) {
+      console.log('Error event:', JSON.stringify(err));
       callback(err);
     }
+    console.log('Received event:', JSON.stringify(event));
     callback(null, event);
   });
+
 };
